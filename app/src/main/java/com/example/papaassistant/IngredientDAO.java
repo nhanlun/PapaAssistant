f6dc5bf21cb8850fb851a02c969ce4bc84d2e37d
nhanlun <ntnhan18@apcs.vn>
2020-09-18 18:24:33 +0700

    Added Room to gradle

---
+++ app/src/main/java/com/example/papaassistant/IngredientDAO.java
@@ -0,0 +1,8 @@
+package com.example.papaassistant;
+
+import androidx.room.Dao;
+
+@Dao
+public interface IngredientDAO {
+
+}
---
+++ app/src/main/java/com/example/papaassistant/Recipe.java
@@ -0,0 +1,9 @@
+package com.example.papaassistant;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    private ArrayList<Ingredient> ingredients;
+    private String name;
+    private int id;
+}
